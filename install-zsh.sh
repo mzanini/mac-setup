bbc9d5d2510e29a782caeeb6fcb67d823a92ace5
Marco Zanini <marco@Marcos-MacBook-Pro.local>
2018-05-30 16:08:15 -0700

    Fix typo in script

--- install-zsh.sh
+++ install-zsh.sh
@@ -6,4 +6,4 @@ echo "Installing Oh-My-Zsh"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 echo "Configuring zsh...."
-sed -i -e 's/_HOME_DIRECTORY_/${HOME}/g' ~/.zshrc
+sed -i -e "s:_HOME_DIRECTORY_:${HOME}:g" ~/.zshrc
