64f0f3d6b1e2da857fc9937c84e4103e5380f590
Marco Zanini <marco@Marcos-MacBook-Pro.local>
2018-05-30 16:48:53 -0700

    Copy new zsh config file before editing in place

--- install-zsh.sh
+++ install-zsh.sh
@@ -6,4 +6,5 @@ echo "Installing Oh-My-Zsh"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 echo "Configuring zsh...."
+cp zshrc ~/.zshrc
 sed -i -e "s:_HOME_DIRECTORY_:${HOME}:g" ~/.zshrc
