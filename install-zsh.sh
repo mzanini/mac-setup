44b93a3f973e6f2c79d8af4d5c6db43cdd3e0f1f
Marco Zanini <marco@Marcos-MacBook-Pro.local>
2018-05-30 16:04:06 -0700

    Add command to configure zsh

--- install-zsh.sh
+++ install-zsh.sh
@@ -4,3 +4,6 @@ brew install zsh
 
 echo "Installing Oh-My-Zsh"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+echo "Configuring zsh...."
+sed -i -e 's/_HOME_DIRECTORY_/${HOME}/g' ~/.zshrc
